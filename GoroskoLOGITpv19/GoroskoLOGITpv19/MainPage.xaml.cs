7c6737b2c9380dccef783000e652eca7f7a733e6
Opilane TTHK <opilane@tthk.ee>
2020-09-18 13:22:40 +0300

    Add project files.

---
+++ GoroskoLOGITpv19/GoroskoLOGITpv19/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GoroskoLOGITpv19
+{
+  
+    public partial class MainPage : ContentPage
+    {
+        bool vkl;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void on_Clicked(object sender, EventArgs e)
+        {
+            vkl = true;
+            while (vkl != false)
+            {
+
+                h.BackgroundColor = Color.Gray;
+                await Task.Run(() => Thread.Sleep(1000));
+                h.BackgroundColor = Color.White;
+                await Task.Run(() => Thread.Sleep(1000));
+                b.BackgroundColor = Color.Aqua;
+                await Task.Run(() => Thread.Sleep(1000));
+                b.BackgroundColor = Color.White;
+                await Task.Run(() => Thread.Sleep(1000));
+                f.BackgroundColor = Color.Aqua;
+                await Task.Run(() => Thread.Sleep(1000));
+                f.BackgroundColor = Color.White;
+                await Task.Run(() => Thread.Sleep(1000));
+            }
+        }
+
+        private void off_Clicked(object sender, EventArgs e)
+        {
+            h.BackgroundColor = Color.White;
+            b.BackgroundColor = Color.White;
+            f.BackgroundColor = Color.White;
+            vkl = false;
+        }
+    }
+}
